24673ad7f321d002056988740e2221d46781a086
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2018-01-17 14:43:55 -0800

    implement touch controls

--- src/input.lua
+++ src/input.lua
@@ -5,8 +5,10 @@ keyMappings = {
     ["space"] = "rotate",
 }
 
+touches = {}
+
 function getInputs()
-    local inputs = {}
+    local inputs = {touches = touches}
     getKeyboardInputs(inputs)
     return inputs
 end
@@ -17,4 +19,40 @@ function getKeyboardInputs(inputs)
             inputs[event] = true
         end
     end
+    for id, touch in pairs(touches) do
+        local swipe = {x = touch.endPos.x - touch.startPos.x,
+                       y = touch.endPos.y - touch.startPos.y}
+        local mag = math.sqrt(swipe.x * swipe.x + swipe.y * swipe.y)
+        if mag > 3 then
+            swipe.x = swipe.x / mag
+            swipe.y = swipe.y / mag
+            local angle = math.atan2(swipe.y, swipe.x)
+            local dir = angle / math.pi
+            if dir < -0.3 and dir > -0.7 then
+                inputs.rotate = true
+            elseif dir > 0.3 and dir < 0.7 then
+                inputs.down = true
+            elseif math.abs(dir) > 0.8 then
+                inputs.left = true
+            elseif math.abs(dir) < 0.2 then
+                inputs.right = true
+            end
+        end
+        break
+    end
+end
+
+function love.touchpressed(id, x, y)
+    touches[id] = {startPos = {x = x, y = y}, endPos = {x = x, y = y}}
+end
+
+function love.touchmoved(id, x, y, dx, dy, pressure)
+    if touches[id] then
+        touches[id].endPos.x = x
+        touches[id].endPos.y = y
+    end
+end
+
+function love.touchreleased(id, x, y)
+    touches[id] = nil
 end
